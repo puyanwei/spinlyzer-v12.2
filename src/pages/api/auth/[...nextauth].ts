fe6cbdb4f5907d41aab5358be06022c12305a088
Puyan Wei <puyanwei@gmail.com>
2022-07-16 17:00:43 +0100

    Put back in prisma adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,14 +8,11 @@ import { prisma } from '../../../server/db/client';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
-  // adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      httpOptions: {
-        timeout: 10000,
-      },
     }),
   ],
   // ...add more providers here
