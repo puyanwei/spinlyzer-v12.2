ec8f5f0ec7dc2751b9069e3f2a1a956504e8b820
Puyan Wei <puyanwei@gmail.com>
2022-07-16 12:08:45 +0100

    Setup Next Auth google sign in

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,35 +1,42 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../server/db/client";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '../../../server/db/client';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     // ...add more providers here
     CredentialsProvider({
-      name: "Credentials",
+      name: 'Credentials',
       credentials: {
         name: {
-          label: "Name",
-          type: "text",
-          placeholder: "Enter your name",
+          label: 'Name',
+          type: 'text',
+          placeholder: 'Enter your name',
         },
       },
       async authorize(credentials, _req) {
-        const user = { id: 1, name: credentials?.name ?? "J Smith" };
+        const user = { id: 1, name: credentials?.name ?? 'J Smith' };
         return user;
       },
     }),
   ],
+  // callbacks: {
+  //   session: async ({ session, user }) => {
+  //     session.id = user.id;
+  //     return Promise.resolve(session);
+  //   },
+  // },
+  secret: process.env.JWT_SECRET,
 };
 
 export default NextAuth(authOptions);
