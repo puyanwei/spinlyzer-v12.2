f1d1248a47bd620b57dbb815abbb06f19c14e150
Puyan Wei <puyanwei@gmail.com>
2022-07-16 16:52:38 +0100

    Added types for build

--- src/pages/api/egg.ts
+++ src/pages/api/egg.ts
@@ -1,6 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next/types';
 import { prisma } from '../../server/db/client';
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const egg = await prisma.example.findMany();
   console.log('egg', egg);
   res.status(200).json(egg);
